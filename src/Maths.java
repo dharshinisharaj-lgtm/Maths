4523833e2cfc8dabb5ecfbb7fac2295a99d042da
ckutraleeswaran <kutraleeswaranc@gmail.com>
2025-11-10 14:53:39 +0530

    i done my addition program

--- src/Maths.java
+++ src/Maths.java
@@ -1,7 +1,15 @@
 public class Maths {
 
+    public void addition(){
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("The c value is = : " + c);
+    }
 
     public static void main(String[] args) {
 
+        Maths obj = new Maths();
+        obj.addition();
     }
 }
